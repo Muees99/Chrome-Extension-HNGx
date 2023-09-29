68fcda79dcb30d9074a546db670c46a80681aef4
Muees99 <132849693+Muees99@users.noreply.github.com>
2023-09-29 07:18:51 -0700

    first commit

---
+++ js/popup.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    // GET THE SELECTORS OF THE BUTTONS
+    const startRecordingButton = document.querySelector("button#start_recording")
+    const stopVideoButton = document.querySelector("button#stop_video")
+
+    // adding event listeners
+
+    startRecordingButton.addEventListener("click", ()=>{
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {action: "request_recording"},  function(response){
+                if(!chrome.runtime.lastError){
+                    console.log(response)
+                } else{
+                    console.log(chrome.runtime.lastError, 'error line 14')
+                }
+            })
+        } )
+    })
+
+
+    stopVideoButton.addEventListener("click", ()=>{
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {action: "stopvideo"},  function(response){
+                if(!chrome.runtime.lastError){
+                    console.log(response)
+                } else{
+                    console.log(chrome.runtime.lastError, 'error line 27')
+                }
+            })
+        } )
+    })
+})
